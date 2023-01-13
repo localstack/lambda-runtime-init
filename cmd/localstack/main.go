80f3c2a740284a3080151e4e30cf131848f1128e
Daniel Fangl <daniel.fangl@localstack.cloud>
2023-01-13 14:34:47 +0100

    add dns rewrite functionality

--- cmd/localstack/main.go
+++ cmd/localstack/main.go
@@ -21,6 +21,8 @@ type LsOpts struct {
 	InitTracingPort   string
 	CodeDownloadUrl   string
 	HotReloadingPaths []string
+	EnableDnsServer   string
+	LocalstackIP      string
 }
 
 func GetEnvOrDie(env string) string {
@@ -41,6 +43,8 @@ func InitLsOpts() *LsOpts {
 		// optional or empty
 		CodeDownloadUrl:   os.Getenv("LOCALSTACK_CODE_ARCHIVE_DOWNLOAD_URL"),
 		HotReloadingPaths: strings.Split(GetenvWithDefault("LOCALSTACK_HOT_RELOADING_PATHS", ""), ","),
+		EnableDnsServer:   os.Getenv("LOCALSTACK_ENABLE_DNS_SERVER"),
+		LocalstackIP:      os.Getenv("LOCALSTACK_HOSTNAME"),
 	}
 }
 
@@ -57,6 +61,9 @@ func main() {
 	log.SetReportCaller(true)
 	// download code archive if env variable is set
 	DownloadCodeArchive(lsOpts.CodeDownloadUrl)
+	// enable dns server
+	dnsServerContext, stopDnsServer := context.WithCancel(context.Background())
+	go RunDNSRewriter(lsOpts, dnsServerContext)
 	// parse CLI args
 	opts, args := getCLIArgs()
 	bootstrap, handler := getBootstrap(args, opts)
@@ -67,6 +74,7 @@ func main() {
 		AddShutdownFunc(func() {
 			log.Debugln("Closing file watcher")
 			cancelFileWatcher()
+			stopDnsServer()
 		}).
 		AddShutdownFunc(func() { os.Exit(0) }).
 		SetExtensionsFlag(true).
--- go.mod
+++ go.mod
@@ -9,9 +9,10 @@ require (
 	github.com/go-chi/render v1.0.1
 	github.com/google/uuid v1.1.2
 	github.com/jessevdk/go-flags v1.4.0
+	github.com/miekg/dns v1.1.50
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
-	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/sys v0.1.0
 )
 
@@ -20,6 +21,9 @@ require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
