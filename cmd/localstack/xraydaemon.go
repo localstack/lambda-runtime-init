9c7f7cce6dba87146552f6e33aca68a2b1eccff4
Dominik Schubert <dominik.schubert91@gmail.com>
2023-03-17 14:53:32 +0100

    bind to localhost

--- cmd/localstack/xraydaemon.go
+++ cmd/localstack/xraydaemon.go
@@ -72,8 +72,8 @@ type Daemon struct {
 
 func initConfig(endpoint string) *cfg.Config {
 	xrayConfig := cfg.DefaultConfig()
-	xrayConfig.Socket.UDPAddress = "0.0.0.0:2000"
-	xrayConfig.Socket.TCPAddress = "0.0.0.0:2000"
+	xrayConfig.Socket.UDPAddress = "127.0.0.1:2000"
+	xrayConfig.Socket.TCPAddress = "127.0.0.1:2000"
 	xrayConfig.Endpoint = endpoint
 	xrayConfig.NoVerifySSL = util.Bool(true) // obvious
 	xrayConfig.LocalMode = util.Bool(true)   // skip EC2 metadata check
@@ -109,7 +109,7 @@ func initDaemon(config *cfg.Config) *Daemon {
 	log.Infof("Initializing AWS X-Ray daemon %v", cfg.Version)
 
 	parameterConfig := cfg.ParameterConfigValue
-	parameterConfig.Processor.BatchSize = 2 // TODO
+	parameterConfig.Processor.BatchSize = 4
 	parameterConfig.Processor.IdleTimeoutMillisecond = 30_000
 	receiveBufferSize := parameterConfig.Socket.BufferSizeKB * 1024
 
@@ -145,7 +145,7 @@ func initDaemon(config *cfg.Config) *Daemon {
 		log.Errorf("Unable to start http proxy server: %v", err)
 		os.Exit(1)
 	}
-	processorCount := 2 // TODO
+	processorCount := 2
 
 	daemon := &Daemon{
 		receiverCount:     parameterConfig.ReceiverRoutines,
