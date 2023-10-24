d176f7dd4f8757bf9c11b622f6406bf01b9715ad
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-10-24 18:51:19 +0200

    Set file permissions of code directory when layer present (#27)

--- cmd/localstack/file_utils.go
+++ cmd/localstack/file_utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -19,4 +20,20 @@ func ChmodRecursively(root string, mode os.FileMode) error {
 			}
 			return nil
 		})
+}
+
+// Check if a directory is empty
+// Source: https://stackoverflow.com/questions/30697324/how-to-check-if-directory-on-path-is-empty/30708914#30708914
+func IsDirEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1) // faster than f.Readdir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
 }
